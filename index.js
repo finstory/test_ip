5df00f5ddce2a1ed85c5eefc24cdec94d0ea9912
finstory <facu995electro@hotmail.com>
2024-06-13 08:44:03 -0300

    add new get info ip

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 
 app.set("trust proxy", true);
@@ -7,7 +12,9 @@ app.use(cors());
 
 app.get('/', async (req, res) => {
     const ip = req.ip;
-    res.send(`Tu IP es: ${ip}`);
+    const url = `https://api.ipdata.co/${ip}?api-key=${process.env.IP_KEY}`;
+    const response = await axios.get(url);
+    res.send(response.data);
 });
 
 const port = 3000;
